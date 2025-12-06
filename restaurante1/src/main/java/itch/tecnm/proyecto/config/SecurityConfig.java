20ba249c5a19f469ec8cc395e698162a5e6c7b82
LeslieRA <138533458+LeslieRA@users.noreply.github.com>
2025-12-05 20:30:27 -0600

    1.r

--- restaurante1/src/main/java/itch/tecnm/proyecto/config/SecurityConfig.java
+++ restaurante1/src/main/java/itch/tecnm/proyecto/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(
             java.util.List.of(
-                "https://frontend-production-5988.up.railway.app"
+                "https://front-production-8086.up.railway.app/"
             )
         );
 
