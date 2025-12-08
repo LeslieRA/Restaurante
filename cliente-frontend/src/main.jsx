773bb50c883644b5fafbd8b836b85c43c4ff5c3f
LeslieRA <138533458+LeslieRA@users.noreply.github.com>
2025-12-08 01:33:49 -0600

    Interfaz 4

--- cliente-frontend/src/main.jsx
+++ cliente-frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
-import 'bootstrap/dist/css/bootstrap.min.css'  // importa bootstrap aquí una sola vez
-import 'bootstrap/dist/js/bootstrap.bundle.min.js' // para que funcione el menú responsive
+//import 'bootstrap/dist/css/bootstrap.min.css'  // importa bootstrap aquí una sola vez
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js' // para que funcione el menú responsive
 
 import './index.css'
 
